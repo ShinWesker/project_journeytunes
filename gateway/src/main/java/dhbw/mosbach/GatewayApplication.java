c7374c9e003dfdfebbbe2b54f444cf38110090a6
ShinWesker <c.becker-privat@outlook.com>
2024-03-03 20:15:11 +0100

    empty gateway swaggerui in gateway

--- gateway/src/main/java/dhbw/mosbach/GatewayApplication.java
+++ gateway/src/main/java/dhbw/mosbach/GatewayApplication.java
@@ -1,5 +1,7 @@
 package dhbw.mosbach;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +9,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "JourneyTunes API Gateway", version = "1.0", description = "Documentation API Gateway v1.0"))
+
 public class GatewayApplication {
 
 	public static void main(String[] args) {
